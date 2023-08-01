e12b8294de4aa243f6abc5af1d6795546672eeff
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-01 09:30:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ var w = document.documentElement.clientWidth;
 var h = document.documentElement.clientHeight;
         
 // put the result into a h1 tag
- document.getElementById('wh').innerHTML = " <div id="sizeInfo"><h1>Width: " + w + " â€¢ Height: " + h + "</h1>"	</div>;
+// document.getElementById('sizeInfo').innerHTML="<h1>Width: " + w + " â€¢ Height: " + h + "</h1>"
+	 document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " â€¢ Height: " + h + "</h1>";
 }
